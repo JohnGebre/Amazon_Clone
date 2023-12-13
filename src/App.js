e56e68055a235b0bdaa8b7a45ed817f9f89ede97
YohannesNi <yohannes.addis03@gmail.com>
2023-12-13 15:06:27 -0500

    Added CheckOut Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import "./App.css";
+import CheckOut from "./components/CheckOut/CheckOut";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+      {/* <Header /> */}
+        <Routes>
+          {/* <Route path="/login" element={<Login/>}/> */}
+          <Route path="/" element={<><Header/> <Home /></>}/>
+          <Route path="/checkout" element={<><Header/><CheckOut/></>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import './CheckOut.css'
+import React from "react";
+import "./CheckOut.css";
 
 function CheckOut() {
   return (
-    <div>CheckOut</div>
-  )
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+        <h3>Hello </h3>
+        <h2 className="checkout__title">Your shopping Basket</h2>
+      </div>
+    </div>
+  );
 }
 
-export default CheckOut
+export default CheckOut;

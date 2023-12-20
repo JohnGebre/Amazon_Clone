cdef54fdadc78253df4315ba4824702a1bf90cb2
YohannesNi <yohannes.addis03@gmail.com>
2023-12-19 22:35:07 -0500

    Edit firebase

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDciowJI4QGlBiiGe1gi605g0kGSIOrVHQ",
-  authDomain: "clone-10ef0.firebaseapp.com",
-  projectId: "clone-10ef0",
-  storageBucket: "clone-10ef0.appspot.com",
-  messagingSenderId: "932610334336",
-  appId: "1:932610334336:web:5249e0a80c76751e4ad065",
-  measurementId: "G-0Z9VJYN590",
+  apiKey: "AIzaSyC7EBkNDhwnSLiGV-axASZDhq6T9B921As",
+  authDomain: "clone-3131f.firebaseapp.com",
+  projectId: "clone-3131f",
+  storageBucket: "clone-3131f.appspot.com",
+  messagingSenderId: "929069704044",
+  appId: "1:929069704044:web:80b31ae23071b60d0a0949",
+  measurementId: "G-DEPZPLQ8NX",
 };
 
 // Initialize Firebase
